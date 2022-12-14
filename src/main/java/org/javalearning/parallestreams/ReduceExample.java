0382a1e214ac4024dbe679d2f05439ae37675285
Himanshu Pandey <himanshupandey2411@gmail.com>
2022-12-15 01:14:45 +0530

    added Reduce

---
+++ src/main/java/org/javalearning/parallestreams/ReduceExample.java
@@ -0,0 +1,22 @@
+package org.javalearning.parallestreams;
+
+import java.util.List;
+
+public class ReduceExample {
+    public  int reduce_sum_ParallelStream(List<Integer> inputList){
+
+        int sum= inputList
+                .parallelStream()
+                .reduce(0, (x,y)->x+y);
+        return sum;
+    }
+
+    public  int reduce_multiply_parallelStream(List<Integer> inputList){
+        int sum= inputList
+                .parallelStream()
+                .reduce(1, (x,y)->x*y);
+        return sum;
+    }
+
+
+}
---
+++ src/test/java/org/javalearning/parallestreams/ReduceExampleTest.java
@@ -0,0 +1,67 @@
+package org.javalearning.parallestreams;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReduceExampleTest {
+
+    ReduceExample reduceExample = new ReduceExample();
+
+    @Test
+    void reduce_ParallelStream(){
+        //given
+        List<Integer> integerList = List.of(1,2,3,4,5,6,7,8);
+
+        //when
+        int result = reduceExample.reduce_sum_ParallelStream(integerList);
+
+        //then
+        assertEquals(36, result);
+    }
+
+    @Test
+    void reduce_ParallelStream_EmptyList(){
+        //given
+        List<Integer> integerList = new ArrayList<>();
+
+        //when
+        int result = reduceExample.reduce_sum_ParallelStream(integerList);
+
+        //then
+        assertEquals(0, result);
+    }
+
+    @Test
+    void reduce_multiply() {
+
+        //given
+        List<Integer> inputList = List.of(1,2,3,4);
+
+        //when
+        int result = reduceExample.reduce_multiply_parallelStream(inputList);
+
+        //then
+        assertEquals(24, result);
+    }
+
+    @Test
+    void reduce_multiply_emptyList() {
+
+        //given
+        List<Integer> inputList = new ArrayList<>();
+
+        //when
+        int result = reduceExample.reduce_multiply_parallelStream(inputList);
+
+        //then
+        assertEquals(1, result);
+    }
+
+
+
+
+}
