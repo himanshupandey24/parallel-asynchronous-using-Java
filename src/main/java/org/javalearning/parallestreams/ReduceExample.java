9167644d9db30edbde0222cbca5a634a14c11a57
Himanshu Pandey <himanshupandey2411@gmail.com>
2022-12-15 01:17:40 +0530

    added Reduce

--- src/main/java/org/javalearning/parallestreams/ReduceExample.java
+++ src/main/java/org/javalearning/parallestreams/ReduceExample.java
@@ -5,17 +5,15 @@ import java.util.List;
 public class ReduceExample {
     public  int reduce_sum_ParallelStream(List<Integer> inputList){
 
-        int sum= inputList
+        return inputList
                 .parallelStream()
-                .reduce(0, (x,y)->x+y);
-        return sum;
+                .reduce(0, Integer::sum);
     }
 
     public  int reduce_multiply_parallelStream(List<Integer> inputList){
-        int sum= inputList
+        return inputList
                 .parallelStream()
                 .reduce(1, (x,y)->x*y);
-        return sum;
     }
 
 
